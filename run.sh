09ac1c5b9ddab16217a60773797a16d59fd75f4a
PeterRain <waloyn@gmail.com>
2021-11-26 11:07:59 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+#Caddy deploy
+mkdir -p /etc/caddy/ /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" >/usr/share/caddy/robots.txt
+wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
+wget -qO- $CONFIGCADDY  >/etc/caddy/Caddyfile
+
 #trojan-go deploy
 mkdir /tmp/trojan-go
 wget -O /tmp/trojan-go/trojan-go.zip https://github.com/p4gefau1t/trojan-go/releases/latest/download/trojan-go-linux-amd64.zip
@@ -12,8 +17,8 @@ cat << EOF > /usr/local/etc/trojan-go/config.yaml
 run-type: server
 local-addr: 0.0.0.0
 local-port: $PORT
-remote-addr: $REMOTE_ADDR
-remote-port: $REMOTE_PORT
+remote-addr: 127.0.0.1
+remote-port: 80
 log-level: 5
 password:
   - $TROJAN_PASSWORD
@@ -34,11 +39,6 @@ transport-plugin:
   enabled: true
   type: plaintext
 EOF
-
-#Caddy deploy
-mkdir -p /etc/caddy/ /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" >/usr/share/caddy/robots.txt
-wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
-wget -qO- $CONFIGCADDY  >/etc/caddy/Caddyfile
 
 /usr/local/bin/trojan-go -config /usr/local/etc/trojan-go/config.yaml &
 caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
